67e6d8631af59c2b6e36ea8f1f84e6f679f7d2cf
Florencia Valdez <florencianoelivaldez@gmail.com>
2023-05-05 09:23:44 -0300

    Añadir modo oscuro

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,36 @@
-import { IoMoonOutline } from 'react-icons/io5'
+import { useEffect, useState } from 'react'
+import { IoMoonOutline, IoMoon } from 'react-icons/io5'
 
 export const Navbar = () => {
+  const [theme, setTheme] = useState('light')
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [theme])
+
   return (
-    <header className='h-16 bg-white shadow-md px-6'>
+    <header className='h-16 bg-white dark:bg-slate-800 dark:text-white shadow-md px-6'>
       <nav className='container mx-auto h-full flex items-center justify-between'>
         <h1 className='text-2xl font-extrabold'>
           Where in th world?
         </h1>
         <div>
-          <button type='button' className='flex items-center gap-2 font-semibold'>
-            <IoMoonOutline className='text-lg' />
+          <button
+            type='button'
+            className='flex items-center gap-2 font-semibold'
+            onClick={handleThemeSwitch}
+          >
+            {theme === 'dark'
+              ? <IoMoon className='text-lg' />
+              : <IoMoonOutline className='text-lg' />}
             <span>Dark Mode</span>
           </button>
         </div>
